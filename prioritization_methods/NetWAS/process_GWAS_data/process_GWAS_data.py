528fa92ac0ca723635982e0c3c28d68b3ae1025d
stijn-arends <stijnarends@live.nl>
2022-08-06 15:36:56 +0200

    Created a class which turns the GWAS data into the correct format.

--- prioritization_methods/NetWAS/process_GWAS_data/process_GWAS_data.py
+++ prioritization_methods/NetWAS/process_GWAS_data/process_GWAS_data.py
@@ -14,7 +14,6 @@ class ExtractVEGASColumns:
     p_val_columns = ["P", "pvalue", "p-value", "p_value"]
     
     def __init__(self):
-        # self.df = df
         self.snp = {"score":0, "name":None}
         self.pval = {"score":0, "name":None}
     
@@ -67,4 +66,57 @@ class ExtractVEGASColumns:
         """
         if self.snp["name"] == None or self.pval["name"] == None:
             raise Exception("Run find column names")
-        return [self.snp["name"], self.pval["name"]] 
+        return [self.snp["name"], self.pval["name"]]
+
+
+
+class PrepGWASData:
+    
+    def __init__(self, file, vegas):
+        self.vegas = vegas
+        self.df = self.prepare_data(Path(file))
+
+        
+    def prepare_data(self, file):
+        
+        df = self.read_data(file)
+        
+        df = self.select_columns(df)
+        
+        # Drop NaN
+        df = self.drop_nan(df)
+        
+        df = self.filter_rs_id(df)
+        
+        df = self.set_index(df)
+        print(df)
+        
+        
+    @staticmethod
+    def read_data(file):
+        return pd.read_csv(file, sep="\t", low_memory=False)
+    
+    def select_columns(self, df):
+        self.vegas.find_column_names(df)
+    
+        cols = self.vegas.get_col_names()
+        
+        # Select SNP ID and p-val column
+        df = df.loc[:, cols]
+        
+        return df
+    
+    @staticmethod
+    def drop_nan(df):
+        return df.dropna()
+    
+    @staticmethod
+    def filter_rs_id(df):
+        return df[df.iloc[:, 0].str.startswith("rs")]
+    
+    @staticmethod
+    def set_index(df):
+        df.set_index(df.iloc[:,0], drop=True, inplace=True)
+        df.drop(columns=df.columns[0], axis=1, inplace=True)
+        df.sort_index(inplace=True)
+        return df
