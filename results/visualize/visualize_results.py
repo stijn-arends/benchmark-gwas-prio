992fbd72f30e684aead490a73aa93e154f6f2023
stijn-arends <stijnarends@live.nl>
2022-08-11 18:35:39 +0200

    Created a seperate folder and python script for visualizing the results.

---
+++ results/visualize/visualize_results.py
@@ -0,0 +1,17 @@
+"""
+This module is used to visualzie the results of comparing several 
+gene prioritization methods.
+"""
+
+
+__author__ = "Stijn Arends"
+__version__ = "v.01"
+__date__ = "11-8-2022"
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
